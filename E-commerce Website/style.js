2fba0a655037cbea893cb5ea5b217a2278a2eb83
Roseduwal <140582455+Roseduwal@users.noreply.github.com>
2023-07-29 22:12:00 +0545

    Add files via upload

---
+++ E-commerce Website/style.js
@@ -0,0 +1,5 @@
+
+// function for the buy buttons
+function but(){
+    alert('We apologize for inconvience,but this website is not functioning yet.We request you to  manaually call our office to place an order.');
+}
